e5858f6182263be3e938edb2ecd13062f96f9c40
jdyeakel <jdyeakel@gmail.com>
2015-06-22 15:02:56 -0600

    ipbc function

--- starvingRW_sim.R
+++ starvingRW_sim.R
@@ -1,60 +1,76 @@
 
 
 #Cell lattice
-#How many cells?
-n <- 1000
-nloc <- seq(1,n,1)
+#How many cells? (total)
+L <- 10
+size <- (L+2)^2
+nloc <- seq(1,size,1)
 #Resource state at each location
-r <- rep(1,n)
-#Resource delay clock
-r_tic <- rep(0,n)
-#delay time
-t_delay <- 5
+r <- rep(1,size)
+
+ipbc <- function(x,L) {
+  check <- 0
+  if (x <= L+2) {new_x <- x + L*(L+2); check <- 1}
+  if (x >= ((L+2)^2 - (L+2))) {new_x <- x - L*(L+2); check <- 1}
+  if (x%%(L+2) == 1) {new_x <- x + (L); check <- 1}
+  if (x%%(L+2) == 0) {new_x <- x - (L); check <- 1}
+  if(check == 0) {new_x <- x}
+  nn <- c(new_x+1,new_x-1,new_x+(L+2),new_x-(L+2))
+  return(nn)
+}
 
 #Maximum energetic state
 s_max <- 50
 s_crit <- 25
+gain <- s_max
+
 #initial number of random walkers
 nrw <- 10
 #Random walker energetic state
 srw <- rep(s_max,nrw)
 
-#Set record keeping slots
 rwloc <- sample(nloc,x,replace=TRUE)
 
 tmax <- 1000
 
+#Should all be written in CPP
 for (t in 1:tmax) {
   
+  #6) RW Move
+  rwloc <- function(rwloc) {}
+  
   # Internal Dynamics
+  
   #1) update s vector and r vector
-  srw <- srw - 1 + r[rwloc]
+  srw <- srw - 1 + r[rwloc]*gain # Minus 1 for prior movement, and + resource for consumption
+  #Boundary conditions... can't be greater than s_max
+  srw <- sapply(srw,function(x){min(s_max,x)})
+  
   #2) update r vector
   r[rwloc] <- r[rwloc] - 1
   
   #3) implement starvation and mortality
-  dead <- which(srw == 0)
+  dead <- which(srw <= 0)
   srw <- srw[-dead]
   
   
   
-  # RW reproduction
-  full <- which(srw > s_crit)
-  srw_new <- srw[full]
+  #4) RW reproduction
+  full <- which(srw > s_crit) #Alternatively, write reproduction as a probability as a function of full
+  srw_new <- srw[full] #begin with same S as parents
   srw <- c(srw,srw_new)
-  rwloc_new <- rwloc[full]
+  rwloc_new <- rwloc[full] #begin at same cell location as parents
   rwloc <- c(rwloc,rwloc_new)
   
   
-  # Resource growth and colonization
-  r_tic <- r_tic + 1
+  #5) Resource growth ~ from NEIGHBORS
+  #The number of nearest neighbors for each resource patch
+  r_nn <- function(r) {}
   # initiate regrowth
-  r_grow <- which(r_tic == t_delay)
+  r_grow <- which(r_nn > 0) #Alternatively, write colonization as a probability as a funciton of r_nn
   r[r_grow] <- 1
-  #Reset clock
-  r_tic[r_grow] <- 0
-  
-  # RW Move
-  rwloc <- FUNCTION
+
+  #mod = %%
+
   
 }
